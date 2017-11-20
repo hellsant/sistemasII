dfbc856b69498bf7b4f0423f8270db0c873b7338
Encinas Geovanny <33643212+gios2013@users.noreply.github.com>
2017-11-20 11:48:05 -0600

    Add files via upload

---
+++ src/app/notas/notas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-notas',
+  templateUrl: './notas.component.html',
+  styleUrls: ['./notas.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NotasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
