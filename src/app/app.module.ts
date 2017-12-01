c113aa5c0f419a4716a91a3e7cbcbb0ee2739305
Encinas Geovanny <33643212+gios2013@users.noreply.github.com>
2017-11-30 23:05:28 -0600

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Http, HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ImportarComponent } from './importar/importar.component';
@@ -8,8 +9,10 @@ import { CursoComponent } from './curso/curso.component';
 import { AlumnoComponent } from './alumno/alumno.component';
 import { ProfesorComponent } from './profesor/profesor.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { APP_ROUTING } from './app.routes';
-
+import { NotasComponent } from './notas/notas.component';
+import { AppRoutingModule } from './app.routes';
+import { ImportarEstudianteComponent } from './importar/importar-estudiante.component';
+import { ImportarNotasComponent } from './notas/importar-notas.component'
 
 @NgModule({
   declarations: [
@@ -18,13 +21,19 @@ import { APP_ROUTING } from './app.routes';
     CursoComponent,
     AlumnoComponent,
     ProfesorComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotasComponent,
+    ImportarNotasComponent,
+    ImportarEstudianteComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,36 @@ import { AlumnoComponent } from './alumno/alumno.component';
 import { ImportarComponent } from './importar/importar.component';
 import { ProfesorComponent } from './profesor/profesor.component';
 import { CursoComponent } from './curso/curso.component';
+import { NotasComponent } from './notas/notas.component';
+import { ImportarEstudianteComponent } from './importar/importar-estudiante.component';
+import { ImportarNotasComponent } from './notas/importar-notas.component'
 
-const ROUTES: Routes = [
-    { path: 'alumno', component: AlumnoComponent },
-    { path: 'importar', component: ImportarComponent },
-    { path: 'curso', component: CursoComponent },
-    { path: 'profesor', component: ProfesorComponent },
-    { path: '**', pathMatch:'full', redirectTo:'alumno' },
+const appRouters: Routes = [
+  { path: 'alumno', component: AlumnoComponent },
+  { path: 'curso', component: CursoComponent },
+  { path: 'profesor', component: ProfesorComponent },
+  {
+    path: 'importar',
+    component: ImportarComponent,
+    children: [
+   //  { path: '', redirectTo: 'importarEstudiante', pathMatch: 'full' },
+   //  { path: 'importarEstudiante', component: ImportarEstudianteComponent },
+    { path: 'importarEstudiante/:id', component: ImportarEstudianteComponent }
+     ]
+  },
+  {
+    path: 'notas',
+    component: NotasComponent,
+    children: [
+   //  { path: '', redirectTo: 'importarEstudiante', pathMatch: 'full' },
+   //  { path: 'importarEstudiante', component: ImportarEstudianteComponent },
+    { path: 'importarNotas/:id', component: ImportarNotasComponent }
+     ]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'alumno' }
 ];
-
-export const APP_ROUTING = RouterModule.forRoot(ROUTES);
+@NgModule({
+  imports: [RouterModule.forRoot(appRouters)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
